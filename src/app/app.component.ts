190360cc7aa86cb7214bde5159a153af9c5566cd
Ragul David <raguldavid1999@gmail.com>
2023-10-31 12:36:44 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SocialAuthService } from "@abacritt/angularx-social-login";
+import { FacebookLoginProvider } from "@abacritt/angularx-social-login";
+import { GoogleLoginProvider } from "@abacritt/angularx-social-login";
+import { IgxInputDirective, IgxSliderComponent } from 'igniteui-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'learnAngular';
+  constructor(private authService: SocialAuthService) { }
+  ngOnInit(): void {
+    this.authService.authState.subscribe((res)=>{
+      console.log(res,'login data');
+    })
+    this.dropdownList = [
+      { id: 1, itemName: 'India' },
+      { id: 2, itemName: 'Singapore' },
+      { id: 3, itemName: 'Australia' },
+      { id: 4, itemName: 'Canada' },
+      { id: 5, itemName: 'South Korea' },
+      { id: 6, itemName: 'Germany' },
+      { id: 7, itemName: 'France' },
+      { id: 8, itemName: 'Russia' },
+      { id: 9, itemName: 'Italy' },
+      { id: 10, itemName: 'Sweden' }
+    ];
+    this.selectedItems = [
+      { id: 2, itemName: 'Singapore' },
+      { id: 3, itemName: 'Australia' },
+      { id: 4, itemName: 'Canada' },
+      { id: 5, itemName: 'South Korea' }
+    ];
+    this.dropdownSettings = {
+      singleSelection: false,
+      text: 'Select Countries',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      enableSearchFilter: true,
+      classes: 'myclass custom-class'
+    };
+  }
+
+  dropdownList:any = [];
+  selectedItems:any = [];
+  dropdownSettings = {};
+  onItemSelect(item: any) {
+    console.log(item);
+    console.log(this.selectedItems);
+  }
+  OnItemDeSelect(item: any) {
+    console.log(item);
+    console.log(this.selectedItems);
+  }
+  onSelectAll(items: any) {
+    console.log(items);
+  }
+  onDeSelectAll(items: any) {
+    console.log(items);
+  }
+  signInWithGoogle(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+
+  formatLabel(value: number): string {
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
+
+    return `${value}`;
+  }
+  // signOut(): void {
+  //   this.authService.signOut();
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,54 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SocialLoginModule, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
+import { GoogleLoginProvider, FacebookLoginProvider } from '@abacritt/angularx-social-login';
+import { GoogleSigninButtonModule } from '@abacritt/angularx-social-login';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { HammerModule } from '@angular/platform-browser';
+import { IgxSliderModule } from 'igniteui-angular';
+import { FormsModule } from '@angular/forms';
+import { ShareModule } from 'ngx-sharebuttons';
+import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularMultiSelectModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocialLoginModule,
+    GoogleSigninButtonModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    HammerModule,
+    IgxSliderModule,
+    FormsModule,
+    ShareModule,
   ],
-  providers: [],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: false,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider('943567520134-pinkp5dbuhf0bsu6plkqke04k6f3vdr2.apps.googleusercontent.com')
+        },
+        {
+          id: FacebookLoginProvider.PROVIDER_ID,
+          provider: new FacebookLoginProvider('707917980899507')
+        }
+      ],
+      onError: (err) => {
+        console.error(err);
+      }
+    } as SocialAuthServiceConfig,
+  }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
